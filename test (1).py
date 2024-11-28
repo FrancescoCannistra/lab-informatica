f1e0e1a9f868bcbd62805bf1422df76a96a50a31
Francesco Cannistrà <francescor.cannistra@davincimilazzo.edu.it>
2024-11-28 08:18:11 +0100

    Add files via upload

---
+++ test (1).py
@@ -0,0 +1,36 @@
+def somma(operando1, operando2) :
+    return operando1 + operando2
+
+
+def sottrazione(minuendo, sottraendo) :
+    return minuendo - sottraendo
+
+def moltiplicazione(moltiplicando, moltiplicatore) :
+    return moltiplicando * moltiplicatore
+
+def divisione(dividendo, divisore) :
+    return dividendo / divisore
+ 
+print("1, somma") 
+print("2, sottrazione")
+print("3, moltiplicazione")
+print("4, divisione")
+
+scelta = int(input())
+
+print("inserisci il primo valore")
+valore1 = int(input())
+print("inserisci il secondo valore")
+valore2 = int(input())
+
+risultato = 0
+if scelta == 1 :
+    risultato = somma(valore1, valore2)
+elif scelta == 2 :
+    risultato = sottrazione(valore1, valore2)
+elif scelta == 3 :
+    risultato = moltiplicazione(valore1, valore2)
+elif scelta == 4 :
+    risultato = divisione(valore1, valore2)
+else :
+    print("operazione non riuscita")
